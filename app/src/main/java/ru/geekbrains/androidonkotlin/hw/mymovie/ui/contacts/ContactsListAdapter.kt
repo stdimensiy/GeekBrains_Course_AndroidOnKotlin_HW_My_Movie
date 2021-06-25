bcff06a21458c1e7811c432b5c2de9f82c37019e
Dmitriy <stdimensiy@yandex.ru>
2021-06-25 18:54:10 +0300

    Изм 9.3 Заполнение и отображение списка контактов в отдельном фрагменте реализовано. - следующий этап, наведение красоты и зачистка кода.

--- app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/contacts/ContactsListAdapter.kt
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/contacts/ContactsListAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ru.geekbrains.androidonkotlin.hw.mymovie.R
+import ru.geekbrains.androidonkotlin.hw.mymovie.domain.Contact
 
 class ContactsListAdapter : RecyclerView.Adapter<ContactsListViewHolder>() {
-    var items: ArrayList<String> = arrayListOf()
+    //var items: ArrayList<String> = arrayListOf("Первый", "Второй", "Третий", "Четвертый", "Пятый", "Шестой", "Седьмой", "Восьмой", "Девятый")
+    var items: MutableList<Contact> = mutableListOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsListViewHolder {
         val root = LayoutInflater.from(parent.context)
             .inflate(R.layout.contacts_list_item, parent, false)
@@ -14,7 +16,8 @@ class ContactsListAdapter : RecyclerView.Adapter<ContactsListViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ContactsListViewHolder, position: Int) {
-        //TODO("Not yet implemented")
+        val item = items[position]
+        holder.textViewNameContact.text = item.displayName
     }
 
     override fun getItemCount(): Int = items.size
--- app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/contacts/ContactsListFragment.kt
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/contacts/ContactsListFragment.kt
@@ -1,6 +1,7 @@
 package ru.geekbrains.androidonkotlin.hw.mymovie.ui.contacts
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.geekbrains.androidonkotlin.hw.mymovie.databinding.ContactsListFragmentBinding
+import ru.geekbrains.androidonkotlin.hw.mymovie.domain.Contact
 
 class ContactsListFragment : Fragment() {
-    private lateinit var adapter: ContactsListAdapter
+    private var adapter: ContactsListAdapter = ContactsListAdapter()
     private val contactsListViewModel: ContactsListViewModel by viewModels {
         ContactsListViewModelFactory(requireActivity().application)
     }
@@ -18,14 +20,33 @@ class ContactsListFragment : Fragment() {
     private val binding get() = _binding!!
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val contentResolver = requireContext().contentResolver
+        val cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+        )
+
+        val contacts = mutableListOf<Contact>()
+        val safeCursor = cursor ?: return
+        while (safeCursor.moveToNext()){
+            val displayName = safeCursor.getString(safeCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+            contacts.add(Contact(displayName))
+        }
+        adapter.items = contacts
+        safeCursor.close()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = ContactsListFragmentBinding.inflate(inflater, container, false)
-        val root = binding.root
-        adapter = ContactsListAdapter()
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
