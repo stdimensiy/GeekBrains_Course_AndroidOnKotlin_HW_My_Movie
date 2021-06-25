e90e190b02207cf4b3fb7707b67d3f8404051a45
Dmitriy <stdimensiy@yandex.ru>
2021-06-25 22:03:42 +0300

    Изм 10.1 Подготовительный этап

---
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/location/LocationFragment.kt
@@ -0,0 +1,32 @@
+package ru.geekbrains.androidonkotlin.hw.mymovie.ui.location
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.geekbrains.androidonkotlin.hw.mymovie.R
+
+class LocationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LocationFragment()
+    }
+
+    private lateinit var viewModel: LocationViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.location_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
