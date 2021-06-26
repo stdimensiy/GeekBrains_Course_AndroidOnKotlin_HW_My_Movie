ba9e1f8ddc3c84a8fbea0964fe50b821b09275c0
Dmitriy <stdimensiy@yandex.ru>
2021-06-26 11:18:41 +0300

    Изм 10.2 Первая часть задания выполнена.  - Разрешения получены,  - координаты получены выведены на экран.

--- app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/location/LocationFragment.kt
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/mymovie/ui/location/LocationFragment.kt
@@ -1,32 +1,78 @@
 package ru.geekbrains.androidonkotlin.hw.mymovie.ui.location
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.geekbrains.androidonkotlin.hw.mymovie.R
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import ru.geekbrains.androidonkotlin.hw.mymovie.databinding.LocationFragmentBinding
 
 class LocationFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LocationFragment()
+    private var _binding: LocationFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val onLocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("Моя проверка", "Сработал слушатель")
+            binding.locationValue.text =
+                "Долгота " + location.latitude.toString() + " и широта " + location.longitude.toString()
+        }
     }
-
     private lateinit var viewModel: LocationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.location_fragment, container, false)
+    ): View {
+        _binding = LocationFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.locationValue.text = "Координаты пользователя"
+        getLocation()
     }
 
+    private fun getLocation() {
+        Log.d("Моя проверка", "Мтартовал getLocation")
+        activity?.let { context ->
+            Log.d("Моя проверка", "Попытка создать локашон менеджер")
+            val locationManager =
+                context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Log.d("Моя проверка", "GPS_PROVIDER создан и велючен")
+                val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+                provider?.let {
+                    // дополнительная проверка разрешений, на всякий случай (требует система)
+                    if (ActivityCompat.checkSelfPermission(
+                            requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // Если вдруг разрешения не получены (не должно срабатывать
+                        // ибо в активити проверка выполнена)
+                        return
+                    }
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        6000L,
+                        100f,
+                        onLocationListener
+                    )
+                }
+            } else {
+                Log.d("Моя проверка", "GPS_PROVIDER НЕ СОЗДАН")
+            }
+        }
+    }
 }
